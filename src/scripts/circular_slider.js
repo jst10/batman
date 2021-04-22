1dd7f7fdcb3ed4500b4f1cd8dcb99f95a36d522f
Jože Kulovic <joze.kulovic@xlab.si>
2021-04-22 20:11:54 +0200

    Playing with events

--- src/scripts/circular_slider.js
+++ src/scripts/circular_slider.js
@@ -1,25 +1,48 @@
 class CircularSlider {
     container = undefined;
     canvas = undefined;
-    color = 0xFFFF00;
+    color = "#F00FFF";
     minValue = 0;
     maxValue = 100;
     step = 10;
-    radius = 30;
+    radius = 170;
+    settingMode = false;
 
     constructor(container) {
         this.container = container;
         this.insertCanvas();
+        this.initListeners();
         this.draw();
     }
+
+    initListeners() {
+        this.canvas.addEventListener("mousemove", (event) => {
+            let cursorX = event.clientX - this.container.offsetLeft;
+            let cursorY = event.clientY - this.container.offsetTop;
+            console.log(cursorX + "/" + cursorY);
+        });
+        this.canvas.addEventListener("mousedown", (event) => {
+            console.log("mouse mousedown")
+        });
+        this.canvas.addEventListener("mouseup", (event) => {
+            console.log("mouse mouseup")
+        });
+        this.canvas.addEventListener("mouseleave", (event) => {
+            console.log("mouse mouseleave")
+        });
+
+    }
+
 
     insertCanvas() {
         this.canvas = document.createElement('canvas');
         this.canvas.id = "CursorLayer";
-        this.canvas.width = 500;
-        this.canvas.height = 500;
+        this.canvas.style.width = '100%';
+        this.canvas.style.height = '100%';
+        this.canvas.width = this.container.offsetWidth;
+        this.canvas.height = this.container.offsetHeight;
         this.canvas.style.zIndex = 8;
-        this.canvas.style.position = "absolute";
+        this.canvas.style.position = "relative";
         this.canvas.style.border = "1px solid";
         this.container.appendChild(this.canvas)
     }
@@ -28,7 +51,9 @@ class CircularSlider {
         console.log("draw method");
         let ctx = this.canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+        ctx.arc(this.canvas.width / 2, this.canvas.height / 2, this.radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 25;
         ctx.stroke();
     }
 }
